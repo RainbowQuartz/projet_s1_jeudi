2e6ce0d970a631670216993931ead4bad6e1dce1
Léona Chevrel <chevrel.leona@gmail.com>
2018-07-05 16:45:49 +0200

    fonction include?(coin)

--- crypto.rb
+++ crypto.rb
@@ -34,14 +34,14 @@ def smallest_value(x)
 
 end
 
-puts "Voici la crypto qui as la plus grosse valeur : " + smallest_value(get_f_values(@crypto_values))
+puts "Voici la crypto qui as la plus petite valeur : " + smallest_value(get_f_values(@crypto_values))
 
 
 def crypto_coin(x)
 	n = 0
-	x.each do
-#		if 	x.has_key?("Coin") || x.has_key?("coin")
-		if x.any? {|k| k.include? "Coin"}
+	x.each do |key, value|
+    key = key.downcase
+		if key.include?("coin")
 			n += 1
 		end
 	end
